385b00c2ed6e793a2962bd0481228bc5353c1b6d
heleeha <helene.e.h@hotmail.com>
2022-06-13 13:50:09 +0200

    update read from ais with time

--- testing_ais.cpp
+++ testing_ais.cpp
@@ -1,23 +1,23 @@
 #include "Eigen/Dense"
-#include "math.h"
-#include <map>
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <sstream>
+#include <iomanip>
+#include <ctime>
 
 void read_ais_to_ship_states(){
     using namespace std;
     vector<Eigen::Vector4d> current_ship_states;
-    ifstream ifile("new_case_LQLVS-60-sec.csv");
+    ifstream ifile("files/new_case_LQLVS-60-sec.csv");
     int ship_mmsi = 248375000; //wanted ship for current states
     vector<int> mmsi_vec;
-    vector<string> time_vec;
+    vector<time_t> time_vec;
     vector<double> x_vec, y_vec, sog_vec, cog_vec;
     int mmsi;
-    string time;
+    time_t time;
     double x, y, sog, cog;
     string str;
     if(ifile.is_open()){
@@ -30,7 +30,17 @@ void read_ais_to_ship_states(){
             string token;
             getline(iss, token, ',');
             mmsi = stoi(token);
-            getline(iss, time, ',');
+            
+            getline(iss, token, ',');
+            struct std::tm td;
+            istringstream ss(token);
+            ss >> std::get_time(&td, "%Y-%m-%d %H:%M:%S"); // or just %T in this case
+            std::time_t time = mktime(&td);
+            std::tm local = *std::localtime(&time);
+            //std::cout << "local: " << std::put_time(&local, "%c %Z") << '\n';
+            //cout << token << std::endl;
+            //cout << time << std::endl;
+            
             getline(iss, token, ',');
             x = stod(token);
             int new_x = round(x);
@@ -42,13 +52,13 @@ void read_ais_to_ship_states(){
             cog = stod(token);
 
             mmsi_vec.push_back(mmsi);
-            time_vec.push_back(time);
             x_vec.push_back(x);
             y_vec.push_back(y);
             sog_vec.push_back(sog);
             cog_vec.push_back(cog);
             
             if(mmsi==ship_mmsi){
+                time_vec.push_back(time);
                 vector<Eigen::VectorXd> ship_states;
                 ship_states.push_back(Eigen::VectorXd(4));
                 ship_states[0] << x,y,sog,cog; 
@@ -60,7 +70,7 @@ void read_ais_to_ship_states(){
     }
      
      
-    for (int i= 0; i <=3; i++){
+    for (int i= 0; i <2; i++){
         cout << "mmsi: " << mmsi_vec[i] << endl;
         cout << "time: " << time_vec[i] << endl;
         cout << "current states: " << current_ship_states[i] << endl;
