009887b1f786be012bdc0a1e4b5d41c468d15bf4
guroveglo <guroveglo@gmail.com>
2022-06-30 12:07:49 +0200

    added ample time distribution

--- intention_model.h
+++ intention_model.h
@@ -227,23 +227,31 @@ namespace INTENTION_INFERENCE
 				}
 			}
 
-			net.setPriorNormalDistribution("intention_ample_time", parameters.ample_time_s.mu, parameters.ample_time_s.sigma, parameters.ample_time_s.max / parameters.ample_time_s.n_bins);
+			//net.setPriorNormalDistribution("intention_ample_time", parameters.ample_time_s.mu, parameters.ample_time_s.sigma, parameters.ample_time_s.max / parameters.ample_time_s.n_bins);
 			//net.setPriorNormalDistribution("intention_safe_distance", parameters.safe_distance_m.mu, parameters.safe_distance_m.sigma, parameters.safe_distance_m.max / parameters.safe_distance_m.n_bins);
 			//net.setPriorNormalDistribution("intention_safe_distance_midpoint", parameters.safe_distance_midpoint_m.mu, parameters.safe_distance_midpoint_m.sigma, parameters.safe_distance_midpoint_m.max / parameters.safe_distance_midpoint_m.n_bins);
 			//net.setPriorNormalDistribution("intention_safe_distance_front", parameters.safe_distance_front_m.mu, parameters.safe_distance_front_m.sigma, parameters.safe_distance_front_m.max / parameters.safe_distance_front_m.n_bins);
-			int colreg_idx = 7;
 			int cpa_ts_idx = 4;  // per nå lik r_maneuver_own (skal byttes til cpa_ts_idx)
 			int cpa_dist_idx = 6;
+			int colreg_idx = 7;
+			int cpa_ample_time_idx = 8;
+
 			
 			int timestep = 60;
 			int n_bins = 30;
 			int multiply =1;
+
 			int head_on = 3;
 			int overtake = -2;
 			int crossing = -1;
-			net.setAisDistribution("intention_safe_distance_midpoint", "classified_west.csv", colreg_idx, cpa_dist_idx, multiply, n_bins, head_on);
-			net.setAisDistribution("intention_safe_distance", "classified_west.csv", colreg_idx, cpa_dist_idx, multiply, n_bins, overtake);
-			net.setAisDistribution("intention_safe_distance_front", "classified_west.csv", colreg_idx, cpa_dist_idx, multiply, n_bins, crossing);
+			int ample_time = 1;
+			
+			// Cpa distance
+			net.setAisDistribution("intention_safe_distance_midpoint", "classified_west_5.csv", colreg_idx, cpa_dist_idx, multiply, n_bins, head_on);
+			net.setAisDistribution("intention_safe_distance", "classified_west_5.csv", colreg_idx, cpa_dist_idx, multiply, n_bins, overtake);
+			net.setAisDistribution("intention_safe_distance_front", "classified_west_5.csv", colreg_idx, cpa_dist_idx, multiply, n_bins, crossing);
+			// Cpa time
+			net.setAisDistribution("intention_ample_time", "classified_west_5.csv", colreg_idx, cpa_ample_time_idx, multiply, n_bins, head_on);
 
 
 		}
